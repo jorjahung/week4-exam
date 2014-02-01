40593f001916a7b9bd3a4720bb37671fe9370691
Jorja Hung <jorjahung@gmail.com>
2014-02-01 12:59:13 +0000

    Refactored task two

--- lib/task2.rb
+++ lib/task2.rb
@@ -13,8 +13,6 @@ class Takeaway
 
 	TIME = (Time.now+1*60*60).strftime("%H:%M") 
 
-
-
 	attr_accessor :order, :total
 	
 	def initialize
@@ -22,31 +20,44 @@ class Takeaway
 		@total = []
 	end
 
-	def place_order(item, quantity, subtotal)
-		raise "We do not have that item" if !correct_item?(item)
-		raise "Wrong subtotal given" if !correct_calculation?(item, quantity, subtotal)
-		order << item
-		total << quantity*subtotal
-		sms		
+	def place_order(items, quantities, client_calculation)
+		client_order = Hash[items.zip(quantities)]
+		check_item(client_order)
+		check_calculation(client_calculation)
 	end
 
 	def sms
-		@account_sid = 'ACe6c81d48df21f8af97caad2e7f7b4f9a'
-		@auth_token = 'c335c900163fb2da27b139fe89de544b'
+		# @account_sid = 'ACe6c81d48df21f8af97caad2e7f7b4f9a'
+		# @auth_token = 'c335c900163fb2da27b139fe89de544b'
 
-		@client = Twilio::REST::Client.new @account_sid, @auth_token
-		@client.account.sms.messages.create(
-	  :from => '+441985250028',
-	  :to => '+447765645760',
-	  :body => "Thank you! Your order was placed and will be delivered before #{TIME}")
-	end
-	
-	def correct_calculation?(item, quantity, subtotal)
-		subtotal == MENU.select {|menu| menu[:food] == item.capitalize}.map { |menu| menu[:price]}[0]*quantity
+		# @client = Twilio::REST::Client.new @account_sid, @auth_token
+		# @client.account.sms.messages.create(
+	 #  :from => '+441985250028',
+	 #  :to => '+447765645760',
+	 #  :body => "Thank you! You ordered:\n  #{order.join(", ").capitalize} \nYour total is #{correct_calculation}. \nYour order will be delivered before #{TIME}.")
 	end
 
-	def correct_item?(item)
-		MENU.any? {|menu| menu[:food] == item.capitalize}
+
+	def check_calculation(client_calculation)
+		if client_calculation != correct_calculation
+			raise "Wrong calculation given" 
+		else 
+		 sms
+		end	
+	end
+
+	def correct_calculation	
+		total.flatten.inject(:+)
+	end
+
+	def check_item(client_order)
+		client_order.each_pair do |item, quantity|
+			raise "We do not have #{item}" if !MENU.any? {|menu| menu[:food] == item.capitalize}
+			order << "#{quantity} #{item}"
+			subtotal = MENU.select {|menu| menu[:food] == item.capitalize}.map { |menu| menu[:price]}*quantity
+			total << subtotal
+		end
+		order.flatten!
 	end
 
 end
